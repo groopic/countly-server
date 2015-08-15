8731145eada002dcc6280bfb6c3b80daa8013e58
Faraz Hassan <faraz@eyedeus.com>
2015-08-15 19:12:24 +0500

    Day wise collections for data storage and daily dumps

--- api/api.js
+++ api/api.js
@@ -89,8 +89,17 @@ function validateAppForWriteAPI(params) {
                 delete eventsToLog[i].sum;
                 delete eventsToLog[i].count;
             }
+            Date.prototype.yyyymmdd = function() {
+                var yyyy = this.getFullYear().toString();
+                var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+                var dd  = this.getDate().toString();
+                return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
+            };
 
-            common.db.collection('ingrainEvents' + params.qstring.app_key).save(eventsToLog, function (err, eventsToLog){
+            var d = new Date();
+            
+
+            common.db.collection('d.yyyymmdd();ingrainEvents' + params.qstring.app_key).save(eventsToLog, function (err, eventsToLog){
                 if(err){
                     common.db.collection('eventsErrors').save({errorMsg: err}, function (err, eventsError){});
                 }
